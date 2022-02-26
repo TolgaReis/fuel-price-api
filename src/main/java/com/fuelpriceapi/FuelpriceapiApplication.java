4472925a3566ef952f7a573fcd9198b69bd86dca
tolgareis <tolgaareis@gmail.com>
2022-02-26 20:22:21 +0300

    Create Project
    Create API project.

---
+++ src/main/java/com/fuelpriceapi/FuelpriceapiApplication.java
@@ -0,0 +1,13 @@
+package com.fuelpriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelpriceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelpriceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuelpriceapi/FuelpriceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuelpriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelpriceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
